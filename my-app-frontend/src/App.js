9bc63f56cdaf2e426dff9d41eafffb138aeba16d
Steven Gutierrez <stevogbmv@gmail.com>
2023-06-10 15:13:14 -0700

    Frontend: Setup Routes and links, basic layout for app

--- my-app-frontend/src/App.js
+++ my-app-frontend/src/App.js
@@ -1,7 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route index element={<Basic />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
+}
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Basic</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+function Basic() {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +46,10 @@ function App() {
       </header>
     </div>
   );
+}
+
+function About() {
+  return <h1>This is the about page!</h1>;
 }
 
 export default App;
